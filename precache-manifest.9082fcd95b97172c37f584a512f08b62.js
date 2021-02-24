1eee0db7586492338aa447053d091719c70477d5
prince chauhan <viprajput2507@gmail.com>
2021-02-23 21:38:14 -0800

    Updates

---
+++ precache-manifest.9082fcd95b97172c37f584a512f08b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01495e07509b53148eba9ba780098dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0352400758b48b4329f",
+    "url": "/static/css/main.52ec989c.chunk.css"
+  },
+  {
+    "revision": "d3f97ce922f04ee45fc1",
+    "url": "/static/js/2.7473decc.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.7473decc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0352400758b48b4329f",
+    "url": "/static/js/main.09a23ca7.chunk.js"
+  },
+  {
+    "revision": "3154f66807a73f30e1c7",
+    "url": "/static/js/runtime-main.36dbbcc8.js"
+  }
+]);
